2626f0a48922b24611325f705788bfc09eceb2d6
Upadhyay-codes <abhishek24448@gmail.com>
2026-03-27 13:04:46 +0530

    binary search practise dsa

--- jj.cpp/jj.cpp
+++ jj.cpp/jj.cpp
@@ -1,14 +1,28 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main () { 
-    int a =3;
-    int* b =&a;
-    //*b = value of b ,,, &a - address of a baiscly &= adress and *=value if they are attach ith some variable . 
-   cout << "the adress of a is " << &a << endl;
-   cout << "the adress of a is " << b << endl;
-   cout << "the value of b is " << *b << endl;
-//    double pointer for ** 
-   int** c = &b;
-   cout << "address of b is"<< c << endl;
-    return 0;
+
+    int binarysearch(vector<int> arr, int tar) {
+        int st=0, end=arr.size()-1;
+
+        while (st <= end) {
+            int mid = (st + end) /2;
+            if(tar > arr[mid]) {
+                st = mid +1;
+            }else if (tar < arr[mid]) {
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+    return -1; 
+} 
+int main () {
+    vector<int> arr1 ={-1,0,3,4,5,9,12};
+    int tar1 ;
+    cout <<"enter tar1:" ;
+    cin >>tar1;
+    cout << binarysearch(arr1, tar1) << endl;  
+    vector<int> arr2 = {-1,0,3,5,9,12};
+    int tar2 =0;
 }
